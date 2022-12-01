6c3e6150419fffbb7dbe004ecf8c6b59f7b80cdd
anil-kumar <anilkumar.eminence@gmail.com>
2022-12-01 16:28:29 +0530

    api change

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
   });
   
 
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("app is listening on port " + port);
 });
